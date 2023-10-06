71d6345b3b2a278a2fff06e74e275af8433b5d59
AlbertMok <239856559@qq.com>
2023-10-06 19:27:41 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # otty-tts-auto_datalabeling
+
+## how to use
+
+**注意**
+开发环境必须安装有conda/miniconda
+
+steps:
+
+```shell
+git clone git@github.com:OttyAI/otty-tts-auto_datalabeling.git
+```
+
+```shell
+cd otty-tts-auto_datalabeling
+```
+
+下载依赖
+
+```shell
+python3 download_dependencies.py 
+```
+
+将处理过的原始数据（清洗过的音频文件放到raw_audio,重命名音频（方便分类
+
+```shell
+python3 rename.py
+```
+
+开始标注
+
+```sh
+python3 auto_DataLabeling_re.py
+
+```
+
+每次标注完成后，都要清理标注
+
+```sh
+python3 clean_list.py
+```
--- download_dependencies.py
+++ download_dependencies.py
@@ -9,6 +9,9 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # 构建虚拟环境的路径
 env_path = os.path.join(current_directory, "env/autolabeling")
 
+# model path
+model_path = os.path.join(current_directory, "Model")
+
 
 def create_conda_env():
     try:
@@ -35,6 +38,15 @@ def install_requirements(requirements_file):
         return False
 
 
+def install_model(model_url):
+    try:
+        subprocess.check_call(["git", "clone", model_url, model_path])
+        print("模型安装成功")
+    except subprocess.CalledProcessError as e:
+        print(f"model安装失败: {e}")
+        return False
+
+
 if __name__ == "__main__":
     current_directory = os.path.dirname(__file__)  # 获取当前文件所在的文件夹绝对路径
     requirements_file = current_directory + \
@@ -43,3 +55,6 @@ if __name__ == "__main__":
     if create_conda_env():
         # 安装依赖
         install_requirements(requirements_file)
+    # 安装模型
+    model_url = "https://www.modelscope.cn/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch.git"
+    install_model(model_url)
